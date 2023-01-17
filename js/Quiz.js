ff12333ace8a4362c94d4cae4469c27333e12a03
samartrigui <116160489+samartrigui@users.noreply.github.com>
2023-01-17 13:03:32 +0100

    Add files via upload

---
+++ js/Quiz.js
@@ -0,0 +1,66 @@
+function resultat(){
+    var score=0;
+    if(document.getElementById('correct1').checked)
+    {
+        score++;
+    }
+    if(document.getElementById('correct2').checked)
+    {
+        score++;
+    }
+    if(document.getElementById('correct3').checked)
+    {
+        score++;
+    }
+    if(document.getElementById('correct4').checked)
+    {
+        score++;
+    }
+    if(document.getElementById('correct5').checked)
+    {
+        score++;
+    }
+    if(document.getElementById('correct6').checked)
+    {
+        score++;
+    }
+    if(document.getElementById('correct7').checked)
+    {
+        score++;
+    }
+    if(document.getElementById('correct8').checked)
+    {
+        score++;
+    }
+    if(document.getElementById('correct9').checked)
+    {
+        score++;
+    }
+    if(document.getElementById('correct10').checked)
+    {
+        score++;
+    }
+    if(document.getElementById('correct11').checked)
+    {
+        score++;
+    }
+    if(document.getElementById('correct12').checked)
+    {
+        score++;
+    }
+    if(document.getElementById('correct13').checked)
+    {
+        score++;
+    }
+    if(document.getElementById('correct14').checked)
+    {
+        score++;
+    }
+    if(document.getElementById('correct15').checked)
+    {
+        score++;
+    }
+    alert("ton score est:" +score)
+
+
+}
